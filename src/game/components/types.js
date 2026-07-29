4087ef9435aa303175056d17a857e91fe6559d01
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-07-29 18:11:29 -0300

    Define initial game component types

---
+++ src/game/components/types.js
@@ -0,0 +1,10 @@
+export const Component = Object.freeze({
+  IDENTITY: "Identity",
+  ABILITY_SCORES: "AbilityScores",
+  HEALTH: "Health",
+  ARMOR_CLASS: "ArmorClass",
+  COMBATANT: "Combatant",
+  CONTROLLER: "Controller",
+  EQUIPMENT: "Equipment",
+  RELATIONSHIP: "Relationship",
+});
