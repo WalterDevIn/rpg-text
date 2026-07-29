fc9bc69312ed18720f167754e93d5ffe186f68db
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-07-29 18:13:05 -0300

    Add initial longsword content definition

---
+++ src/content/items/longsword.js
@@ -0,0 +1,10 @@
+export const longsword = Object.freeze({
+  id: "longsword",
+  name: "Longsword",
+  attack: {
+    attackBonus: 5,
+    damageDie: 8,
+    damageBonus: 3,
+    damageType: "SLASHING",
+  },
+});
