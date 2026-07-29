0331761f8be6b695a6f74cf70994fb10891be2f7
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-07-29 18:22:29 -0300

    Equip fighter through reusable content definitions

--- src/content/characters/humanFighter.js
+++ src/content/characters/humanFighter.js
@@ -1,12 +1,14 @@
 import { longsword } from "../items/longsword.js";
+import { chainMail } from "../items/chainMail.js";
+import { shield } from "../items/shield.js";
 
 export const humanFighter = Object.freeze({
   id: "human-fighter",
   kind: "character",
   name: "Walter",
   hitPoints: 14,
-  armorClass: 16,
   initiativeBonus: 2,
+  proficiencyBonus: 2,
   abilityScores: {
     strength: 16,
     dexterity: 14,
@@ -15,8 +17,10 @@ export const humanFighter = Object.freeze({
     wisdom: 12,
     charisma: 10,
   },
-  attack: longsword.attack,
-  weaponId: longsword.id,
-  controller: "player",
+  inventory: [longsword, chainMail, shield],
+  weapon: longsword,
+  armor: chainMail,
+  shield,
+  controller: "manual",
   faction: "heroes",
 });
