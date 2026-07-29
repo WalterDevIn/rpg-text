99f6f1f8baada86be277dfb8a5a78ac0fe738827
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-07-29 18:22:16 -0300

    Add dagger content

---
+++ src/content/items/dagger.js
@@ -0,0 +1,6 @@
+export const dagger = Object.freeze({
+  id: "dagger",
+  name: "Dagger",
+  kind: "weapon",
+  attack: Object.freeze({ attackBonus: 4, damageDie: 4, damageBonus: 2, damageType: "piercing" }),
+});
