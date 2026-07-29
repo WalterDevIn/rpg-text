b1c0b0db5add239fa58a209f22ebf89c244bf3fd
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-07-29 18:22:27 -0300

    Add quarterstaff content

---
+++ src/content/items/quarterstaff.js
@@ -0,0 +1,6 @@
+export const quarterstaff = Object.freeze({
+  id: "quarterstaff",
+  name: "Quarterstaff",
+  kind: "weapon",
+  attack: Object.freeze({ attackBonus: 3, damageDie: 6, damageBonus: 1, damageType: "bludgeoning" }),
+});
