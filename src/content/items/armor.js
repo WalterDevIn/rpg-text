afdb18fd798b9a1e86055b988f051d6051167ef4
WalterDevIn <144739473+WalterDevIn@users.noreply.github.com>
2026-07-29 18:22:48 -0300

    Add armor and shield content

---
+++ src/content/items/armor.js
@@ -0,0 +1,3 @@
+export const leatherArmor = Object.freeze({ id: "leather-armor", name: "Leather armor", kind: "armor", armorClass: 11 });
+export const chainMail = Object.freeze({ id: "chain-mail", name: "Chain mail", kind: "armor", armorClass: 16 });
+export const shield = Object.freeze({ id: "shield", name: "Shield", kind: "shield", armorClassBonus: 2 });
